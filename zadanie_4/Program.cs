083a319feb5f84ce60237c7674abc293d9f41004
Marat <brutto061@gmail.com>
2022-12-02 21:47:11 +0700

    Временное выполнение задания №4

---
+++ zadanie_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Сколько показать первых чисел последовательности? ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int PokazChisel(int n)
+{
+    if (n == a || n == b) return n;
+    else return PokazChisel(n - 1) + PokazChisel(n - 2);
+}
+//Console.Write($"{PokazChisel(N)}");
+for (int i = a; i <= n+2; i++)
+{
+    Console.Write($"{PokazChisel(i)} ");
+}
