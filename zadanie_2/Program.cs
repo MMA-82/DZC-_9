f4a1ddd689b5e3c47ec88def33b3486c1dce6a8a
Marat <brutto061@gmail.com>
2022-12-02 20:09:20 +0700

    Выполнили задание №2

---
+++ zadanie_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+
+void PrintNumbers(int n)
+{
+    if (n >= M)
+    {
+        PrintNumbers(n - 1);
+        Console.Write($"{n} ");
+    }
+}
+PrintNumbers(N);
+Console.WriteLine();
+
+int SumNumbers(int n)
+{
+    if (n == M) return M;
+    else return n + SumNumbers(n - 1);
+}
+Console.Write("Сумма чисел от M до N равна: " + SumNumbers(N));
+
