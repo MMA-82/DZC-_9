167cd8f5a83a6c86256e506d9b699c131cffe660
Marat <brutto061@gmail.com>
2022-12-03 14:19:06 +0700

    не совсем то, что хотелось

--- zadanie_4/Program.cs
+++ zadanie_4/Program.cs
@@ -3,15 +3,15 @@ int a = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine() ?? "0");
 
-Console.Write("Сколько показать первых чисел последовательности? ");
-int n = int.Parse(Console.ReadLine() ?? "0");
 int PokazChisel(int n)
 {
-    if (n == a || n == b) return n;
+    if (n<=a) return n;
     else return PokazChisel(n - 1) + PokazChisel(n - 2);
 }
 //Console.Write($"{PokazChisel(N)}");
-for (int i = a; i <= n+2; i++)
+Console.Write("Сколько показать первых чисел последовательности? ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i <= N; i++)
 {
     Console.Write($"{PokazChisel(i)} ");
 }
